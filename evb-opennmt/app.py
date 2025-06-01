b9d12932e7d0223e5e94eb4408e09e550e57ba1a
5HoSaZ <ttzoththd@gmail.com>
2025-06-01 22:27:32 +0700

    opennmt upload

---
+++ evb-opennmt/app.py
@@ -0,0 +1,44 @@
+import ctranslate2
+import streamlit as st
+import sentencepiece as spm
+import nltk
+from nltk import sent_tokenize
+import torch
+
+torch.classes.__path__ = []
+nltk.download("punkt_tab", quiet=True)
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+ct_model_path = "evb-opennmt/model"
+sp_source_model_path = "evb-opennmt/model/source.model"
+sp_target_model_path = "evb-opennmt/model/target.model"
+
+translator = ctranslate2.Translator(ct_model_path, DEVICE)
+sp_source_model = spm.SentencePieceProcessor(sp_source_model_path)
+sp_target_model = spm.SentencePieceProcessor(sp_target_model_path)
+
+
+st.set_page_config(page_title="OpenNMT", page_icon="📖")
+st.title("OpenNMT Translate")
+
+# Form to add your items
+with st.form("my_form"):
+    # Text area for source text
+    user_input = st.text_area(
+        "Source Text", max_chars=200, placeholder="Example: I have a cat!"
+    )
+    source_sentences = sent_tokenize(user_input)
+    # Encode source tokens
+    source_tokenized = sp_source_model.encode(source_sentences, out_type=str)
+    # Translate
+    translations = translator.translate_batch(source_tokenized)
+    translations = [translation.hypotheses[0] for translation in translations]
+    # Decode target tokens
+    translations_detokenized = sp_target_model.decode(translations)
+    translation = " ".join(translations_detokenized)
+
+    submitted = st.form_submit_button("Translate")
+    if submitted:
+        st.write("Translation")
+        st.info(translation)
